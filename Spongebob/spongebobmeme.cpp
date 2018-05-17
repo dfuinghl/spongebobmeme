7fcc2c7cfbf81a62d5b0ef4c57f3d659b17ca52f
dfuinghl <33442272+dfuinghl@users.noreply.github.com>
2018-05-17 18:31:56 +0800

    Add files via upload

---
+++ Spongebob/spongebobmeme.cpp
@@ -0,0 +1,54 @@
+//changing a string into Spongebob Mocking meme
+
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main()
+
+{
+	srand (time(NULL));
+	char x [50];
+	char word;
+
+	int counter;
+	counter  = rand () % 2;
+	bool carryon = true;
+	char answer;
+
+	while (carryon)
+	{
+		cout << "Enter string: ";
+		cin.getline(x, 100, '\n');
+		int length = strlen(x);
+		cin.clear();
+		
+		for (int i = 0; i < length; i++)
+		{
+			if (counter%2)
+			{
+				word = x[i];
+				x[i] = tolower(word);
+			}
+			else if (!(counter%2))
+			{
+				word = x[i];
+				x[i] = toupper(word);
+			}
+			counter++;
+		}
+		cout << x << endl;
+		cout << "Continue? (Y/N): ";
+		cin >> answer;
+		cin.clear();
+		cin.ignore();
+		if (answer == 'Y' || answer == 'y')
+			carryon = true;
+		else
+			carryon = false;
+	}
+}
+
